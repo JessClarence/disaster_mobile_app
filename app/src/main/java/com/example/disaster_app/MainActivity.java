3fe89a468f32038d24e497196f9537b57d75e6be
Jessc <Jessc@DESKTOP-G6KJNEA>
2024-05-24 21:51:51 +0800

    first commit

---
+++ app/src/main/java/com/example/disaster_app/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.disaster_app;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+    }
+
+    public void openManual(View view) {
+        startActivity(new Intent(MainActivity.this, Manual.class));
+    }
+
+    public void closeApp(View view) {
+        finish();
+    }
+}
---
+++ app/src/main/java/com/example/disaster_app/Manual.java
@@ -0,0 +1,69 @@
+package com.example.disaster_app;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+
+public class Manual extends AppCompatActivity {
+
+    private int[] drawableResources = {
+            R.drawable.manual_1,
+            R.drawable.manual_2,
+            R.drawable.manual_3,
+            R.drawable.manual_4,
+            R.drawable.manual_5,
+            R.drawable.manual_6,
+            R.drawable.manual_7,
+            R.drawable.manual_8,
+            R.drawable.manual_9,
+            R.drawable.manual_10,
+            R.drawable.manual_11,
+            R.drawable.manual_12,
+            R.drawable.manual_13,
+            R.drawable.manual_14};
+    private int i = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manual);
+
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+    }
+
+    public void pageNext(View view) {
+        ConstraintLayout layout = findViewById(R.id.main);
+
+
+        if (i < drawableResources.length) {
+            layout.setBackgroundResource(drawableResources[i]);
+            i++;
+        } else {
+            i = 0;
+        }
+    }
+
+    public void pagePrev(View view) {
+        ConstraintLayout layout = findViewById(R.id.main);
+
+        if (i > 0) {
+            i--;
+        } else {
+            i = drawableResources.length - 1;
+        }
+        layout.setBackgroundResource(drawableResources[i]);
+    }
+
+    public void pageBack(View view) {
+        startActivity(new Intent(Manual.this, MainActivity.class));
+    }
+}
