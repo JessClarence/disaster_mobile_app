a2518a540338b1604801f2d8ec6d19fd4de5ab31
Jessc <Jessc@DESKTOP-G6KJNEA>
2024-06-17 22:18:50 +0800

    new fire

--- app/src/main/java/com/example/disaster_app/FireStory.java
+++ app/src/main/java/com/example/disaster_app/FireStory.java
@@ -7,12 +7,31 @@ import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class FireStory extends AppCompatActivity {
-
+    private int[] drawableResources = {
+            R.drawable.frame1,
+            R.drawable.frame2,
+            R.drawable.frame3,
+            R.drawable.frame4,
+            R.drawable.frame4_1,
+            R.drawable.frame4_2,
+            R.drawable.frame4_3,
+            R.drawable.frame5,
+            R.drawable.frame6,
+            R.drawable.frame7,
+            R.drawable.frame8,
+            R.drawable.frame9,
+            R.drawable.frame10,
+            R.drawable.frame11,
+            R.drawable.frame12,
+            R.drawable.frame13,
+            R.drawable.frame14};
+    private int i = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +45,13 @@ public class FireStory extends AppCompatActivity {
     }
 
     public void nextPage(View view) {
+        ConstraintLayout layout = findViewById(R.id.main);
 
+        if (i < drawableResources.length) {
+            layout.setBackgroundResource(drawableResources[i]);
+            i++;
+        } else {
+            i = 0;
+        }
     }
 }
